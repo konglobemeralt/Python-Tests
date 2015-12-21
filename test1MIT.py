ce7d713127ecf53af77c163c7b89ca7811864698
konglobemeralt <jesper.blidkvist@live.se>
2015-12-21 11:41:57 +0100

    Dummy input test

---
+++ test1MIT.py
@@ -0,0 +1,10 @@
+##thing asking for name
+
+dateOfBirth = ""
+lastName = ""
+
+dateOfBirth = (raw_input("When is your birthday? "))
+lastName = (raw_input("What is your last name? "))
+
+print "Your birthday is: " + dateOfBirth
+print "Your last name is: " + lastName
